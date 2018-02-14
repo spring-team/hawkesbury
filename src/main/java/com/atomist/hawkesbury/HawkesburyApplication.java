ccb781c8c839674e85a5252d2ca4dae8a15a8299
Rod Johnson <johnsonroda@gmail.com>
2018-02-14 21:45:20 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/hawkesbury/HawkesburyApplication.java
@@ -0,0 +1,12 @@
+package com.atomist.hawkesbury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawkesburyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HawkesburyApplication.class, args);
+	}
+}
